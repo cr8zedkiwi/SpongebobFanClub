9f6e3355ef544f7fc4914fefee78a7e64f16f210
kiwic8 <cr8zedkiwi@gmail.com>
2024-10-13 23:59:52 -0400

    Add project files.

---
+++ Models/CustomerCity.cs
@@ -0,0 +1,9 @@
+﻿namespace SongebobFanClub.Models
+{
+    public class CustomerCity
+    {
+        public int CustomerCityId { get; set; }
+
+        public string? City { get; set; }
+    }
+}
---
+++ Models/Employees.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SongebobFanClub.Models
+{
+    public class Employees
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage ="Please enter your full name using 50 characters or less.")]
+        public string? FirstName { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "Please enter your full name using 50 characters or less.")]
+        public string? LastName { get; set; }
+
+        [Required]
+        [StringLength(20, ErrorMessage = "Please enter your position at work using 20 characters or less.")]
+        public string? Position { get; set; }
+
+        [Required]
+        [StringLength(10, ErrorMessage = "Please enter your salary using 10 characters or less.")]
+        public string? Salary { get; set; }
+    }
+}
